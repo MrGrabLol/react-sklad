a3a8be067116fa53617cadd0f65f1a1686d1a003
Sergey Valavin <serh.valavin@gmail.com>
2022-12-07 20:37:57 +0300

    Add printable component

---
+++ src/components/PrintCard.tsx
@@ -0,0 +1,12 @@
+import ReactToPrint from "react-to-print";
+
+export function PrintCard({id} : string) {
+
+    return (
+        <div>
+            <ReactToPrint
+                /
+        </div>
+    )
+
+}
